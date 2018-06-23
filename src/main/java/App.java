3272486e91df79a0d3a69c4a4cf3973035d4bf88
naota <13465771+naota1@users.noreply.github.com>
2018-06-23 14:41:34 +0200

    v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,15 @@
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import stringsort.Sort;
+
+public class App {
+    public static void main(String[] args) {
+        String[] actualNames = new String[]{
+                "Johnson", "Wilson",
+                "Wilkinson", "Abraham", "Dagobert"
+        };
+        final Sort sorter = new Sort();
+        sorter.sort(actualNames);
+        for (final String name : actualNames) {
+            System.out.println(name);
+        }
     }
-}
-
-
 }
--- src/main/java/stringsort/Sort.java
+++ src/main/java/stringsort/Sort.java
@@ -1,4 +1,21 @@
 package stringsort;
 
 public class Sort {
+
+    public void sort(String[] names) {
+
+        int n = names.length;
+
+        while (n > 1) {
+            for (int j = 0; j < n - 1; j++) {
+                if (names[j].compareTo(names[j + 1]) > 0) {
+                    final String tmp = names[j + 1];
+                    names[j + 1] = names[j];
+                    names[j] = tmp;
+                }
+            }
+            n--;
+        }
+
+    }
 }
